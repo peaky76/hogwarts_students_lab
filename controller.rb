bf405c30ecfd1b272ad6db86dbaf4f1d81b26c9b
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-08 15:54:34 +0100

    created view to show all students

--- controller.rb
+++ controller.rb
@@ -3,3 +3,9 @@ require('sinatra/contrib/all')
 
 require_relative('./models/student')
 also_reload('./models/*')
+
+# INDEX
+get '/students' do
+    @students = Student.find_all()
+    erb(:index)
+end
